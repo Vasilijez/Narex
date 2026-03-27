be30570fc35faf7976a912fff90cd08fcded538c
Vasilije <vasilijezzzz@gmail.com>
2026-03-27 21:51:45 +0100

    feat: Support inline rules as well as `OneOf`, `Letter` and `Maybe`

--- tests/test_boilerplate.py
+++ tests/test_boilerplate.py
@@ -11,3 +11,18 @@ def test_literal():
     r = generate(m)
 
     assert r == "Python regex"
+
+def test_inline_rule():
+    m = """
+        clause1:
+            one of 'something' letter
+            maybe one of 'something2' maybe letter  maybe one of 'something3'
+
+        clause2:
+            one of 'yey' letter
+            one of 'wewe' maybe letter  maybe one of 'popopo'
+    """
+
+    mm = get_metamodel()
+    m = mm.model_from_str(m)
+    r = generate(m)
