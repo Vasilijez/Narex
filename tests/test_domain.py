59191dadd45608197596ff49686b4c31f2d21433
Vasilije <vasilijezzzz@gmail.com>
2026-03-28 16:07:20 +0100

    test: Write tests for `Domain` rule

---
+++ tests/test_domain.py
@@ -0,0 +1,119 @@
+from narex import get_metamodel
+from narex.generators.python import generate
+from textx import TextXSyntaxError
+
+def test_domain_domain_types():
+    m = """
+        c1:
+            digit
+            space
+            whitespace
+            alphanumeric
+            anything
+            letter
+            small_letter
+            big_letter
+
+        c1
+    """
+
+    mm = get_metamodel()
+    m = mm.model_from_str(m)
+    r = generate(m)
+
+def test_domain_with_not():
+    m = """
+        c1:
+            not whitespace
+        c2:
+            not anything not letter
+        
+        c1
+    """
+
+    mm = get_metamodel()
+    m = mm.model_from_str(m)
+    r = generate(m)
+
+def test_domain_with_between():
+    m = """
+        c1:
+            digit between 1 and 3
+        c2:
+            letter between a and z
+        c3:
+            letter between A and Z
+        
+        c1
+    """
+
+    mm = get_metamodel()
+    m = mm.model_from_str(m)
+    r = generate(m)
+
+def test_domain_forbidden_rules_before_between():
+    mm = get_metamodel()
+
+    m = """
+        c:
+            space between 1 and 3
+        c
+    """
+    m = mm.model_from_str(m)
+    try:
+        m = mm.model_from_str(m)
+    except Exception as e:
+        assert isinstance(e, TextXSyntaxError)
+
+    m = """
+        c:
+            whitespace between 1 and 3
+        c
+    """
+    m = mm.model_from_str(m)
+    try:
+        m = mm.model_from_str(m)
+    except Exception as e:
+        assert isinstance(e, TextXSyntaxError)
+
+    m = """
+        c:
+            alphanumeric between 1 and 3
+        c
+    """
+    m = mm.model_from_str(m)
+    try:
+        m = mm.model_from_str(m)
+    except Exception as e:
+        assert isinstance(e, TextXSyntaxError)
+
+    m = """
+        c:
+            anything between 1 and 3
+        c
+    """
+    m = mm.model_from_str(m)
+    try:
+        m = mm.model_from_str(m)
+    except Exception as e:
+        assert isinstance(e, TextXSyntaxError)
+
+
+def test_domain_complex():
+    m = """
+        c1:
+            digit between 1 and 3
+        c2:
+            letter between a and z digit between 2 and 4
+        c3:
+            big_letter between A and Z
+        c4:
+            small_letter between a and z
+            digit between 1 and 9
+        
+        c1
+    """
+
+    mm = get_metamodel()
+    m = mm.model_from_str(m)
+    r = generate(m)
