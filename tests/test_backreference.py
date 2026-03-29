49f24c1ad24f307a7abc42897e2d574e85eefd7e
Vasilije <vasilijezzzz@gmail.com>
2026-03-29 18:03:37 +0200

    feat: `Backreference` previously created groups

---
+++ tests/test_backreference.py
@@ -0,0 +1,23 @@
+from narex import get_metamodel
+from narex.generators.python import generate
+
+def test_backreference():
+    m = """
+        c1:
+            group g1 of digit repeat 1 or more times
+            'test'
+        c2:
+            backreference g1
+        c: 
+            c1
+            c2
+
+        c
+    """
+
+    mm = get_metamodel()
+    m = mm.model_from_str(m)
+    r = generate(m)
+
+
+# test_backreference(): bad case when the group doesn't exist
