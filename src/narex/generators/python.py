ad4697fc378d775042219c198a7bf9f9eac201e4
Vasilije <vasilijezzzz@gmail.com>
2026-04-07 22:27:32 +0200

    feat: Start interpreting model by adding `interpret_clause`

--- src/narex/generators/python.py
+++ src/narex/generators/python.py
@@ -4,8 +4,25 @@
 #### Python flavor ####
 #######################
 
+regex = ''
+clauses = {}
+def interpret_clause(c) -> str:
+    if c.clauses:
+        for clause in c.clauses:
+            clauses[clause.name] = interpret_clause(c)
 
-def generate(model):
+    for inline_rule in c.inline_rules:
+        # We can have more than one rule in one `InlineRule`.
+        for rule in inline_rule.rules:
+            interpret_rule(rule)
+
+def generate(model) -> str:
+    for clause in model.clauses:
+        print(f"clause {clause}")
+        interpret_clause(clause)
+
     return f"Python regex"
 
 
+if __name__ == '__main__':
+    ...
