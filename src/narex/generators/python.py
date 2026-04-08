1c1a3860ff069ddcad80c66df25e32717ad28aa2
Vasilije <vasilijezzzz@gmail.com>
2026-04-08 21:23:37 +0200

    feat: Complete generation of `Domain` rule

--- src/narex/generators/python.py
+++ src/narex/generators/python.py
@@ -4,6 +4,15 @@ import string
 #### Python flavor ####
 #######################
 
+simple_domain = {
+    'space', 'whitespace', 'alphanumeric', 
+    'anything', 'letter'
+}
+
+bounded_domain = {
+    'digit', 'small_letter', 'big_letter'
+}
+
 class PythonEngine:
     
     def __init__(self):
@@ -19,14 +28,44 @@ class PythonEngine:
         def end():
             return "]"
 
-    class Digit:
+    class SimpleDomain:
         @staticmethod
-        def without_between():
-            return "\d"
+        def intepret(d):
+            match d.type:
+                case 'space':
+                    return " "
+                case 'whitespace':
+                    return "\s"
+                case 'alphanumeric':
+                    return "\w"
+                case 'anything':
+                    return "."
+                case 'letter':
+                    return "[A-Za-z]"
+                
+    class BoundedDomain:
+        @staticmethod
+        def without_between(d):
+            match d.type:
+                case 'digit':
+                    return "\d"
+                
+                case 'small_letter':
+                    return "[a-z]"
+                
+                case 'big_letter':
+                    return "[A-Z]"
 
         @staticmethod
-        def with_between(start, end):
-            return f"[{start}-{end}]"
+        def with_between(d):
+            return f"[{d.between.start}-{d.between.end}]"
+        
+        @staticmethod
+        def intepret(d):
+            if d.between is None:
+                return PythonEngine.BoundedDomain.without_between(d)
+            return PythonEngine.BoundedDomain.with_between(d)
+
 
     def interpret_one_of(self, o) -> str:
         return f"[{o.set}]"
@@ -36,15 +75,10 @@ class PythonEngine:
         if d.negation is not None:
             self.regex = self.Not.start() + self.regex
         
-        match d.type:
-            case 'digit':
-                if d.between is None:
-                    self.regex = self.regex + self.Digit.without_between()
-                else:               
-                    self.regex = self.regex + self.Digit.with_between(
-                            d.between.start, 
-                            d.between.end
-                        )
+        if d.type in bounded_domain:
+            self.regex += self.BoundedDomain.intepret(d)
+        elif d.type in simple_domain:
+            self.regex += self.SimpleDomain.intepret(d)
         
     def interpret_rule(self, r) -> str:
 
