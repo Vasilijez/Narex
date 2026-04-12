da08c71671d10c62c72b14c67eecd03847eb9eca
Vasilije <vasilijezzzz@gmail.com>
2026-04-12 19:40:22 +0200

    test: Write tests for `Repeat`

--- tests/test_repeat.py
+++ tests/test_repeat.py
@@ -51,3 +51,114 @@ def test_repeat_invalid_missing_subject_rule():
     except Exception as e:
         assert isinstance(e, TextXSyntaxError)
 
+def test_repeat_start_and_end_valid_cases():
+    m = """
+    c {
+        repeat 1 times
+        repeat 2 times
+        repeat 1 to 2 times
+        repeat 1 to 15 times
+        repeat 0 or more times
+        repeat 1 or more times
+        repeat 2 or more times
+    }
+    
+    target:
+        c
+    """
+    mm = get_metamodel()
+    try:
+        m = mm.model_from_str(m)
+    except Exception as e:
+        assert isinstance(e, TextXSyntaxError)
+
+def test_repeat_invalid_0_times():
+    m = """
+    c {
+        repeat 0 times
+    }
+    
+    target:
+        c
+    """
+    mm = get_metamodel()
+    try:
+        m = mm.model_from_str(m)
+    except Exception as e:
+        assert isinstance(e, TextXSyntaxError)
+
+def test_repeat_invalid_0_to_0_times():
+    m = """
+    c {
+        repeat 0 to 0 times
+    }
+    
+    target:
+        c
+    """
+    mm = get_metamodel()
+    try:
+        m = mm.model_from_str(m)
+    except Exception as e:
+        assert isinstance(e, TextXSyntaxError)
+
+def test_repeat_invalid_start_bigger_than_end():
+    m = """
+    c {
+        repeat 5 to 1 times
+    }
+    
+    target:
+        c
+    """
+    mm = get_metamodel()
+    try:
+        m = mm.model_from_str(m)
+    except Exception as e:
+        assert isinstance(e, TextXSyntaxError)
+
+def test_repeat_invalid_negative_start():
+    m = """
+    c {
+        repeat -1 to 0 times
+    }
+    
+    target:
+        c
+    """
+    mm = get_metamodel()
+    try:
+        m = mm.model_from_str(m)
+    except Exception as e:
+        assert isinstance(e, TextXSyntaxError)
+
+def test_repeat_invalid_both_negative():
+    m = """
+    c {
+        repeat -1 to -1 times
+    }
+    
+    target:
+        c
+    """
+    mm = get_metamodel()
+    try:
+        m = mm.model_from_str(m)
+    except Exception as e:
+        assert isinstance(e, TextXSyntaxError)
+
+def test_repeat_invalid_2_to_2_times():
+    m = """
+    c {
+        repeat -1 to 0 times
+    }
+    
+    target:
+        c
+    """
+    mm = get_metamodel()
+    try:
+        m = mm.model_from_str(m)
+    except Exception as e:
+        assert isinstance(e, TextXSyntaxError)
+
