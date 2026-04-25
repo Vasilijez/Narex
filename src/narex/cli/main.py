160ae7b8a54fbd2a2184901aaa8372f38e324846
Vasilije <vasilijezzzz@gmail.com>
2026-04-25 18:48:07 +0200

    feat: Show info on validate command, beautify CLI & add `--full` option

--- src/narex/cli/main.py
+++ src/narex/cli/main.py
@@ -9,24 +9,26 @@ def cli():
 
 @click.command()
 @click.option('--path', default='', help='Enter path to model file')
-def run_command(path):
+@click.option('--full', is_flag=True, help='Generate full code to an output file')
+def run_command(path, full):
     try:
         mm, m = load_metamodel_and_model_path(path)
         e = PythonEngine()
-        result = e.generate(m)
-        print(f"Result {result}")
+        result = e.generate(m, False) if full else e.generate(m)
+        click.secho(click.style(f"Raw regex output: {result}", fg='green'))
+        if full:
+            click.secho(click.style(f"Full code generated and saved to: /src/narex/generators/out_regex.py", fg='blue'))
     except Exception as e:
-        print(f"An error occured while running the model: \n{e}")
-
+        click.secho(click.style(f"An error occured while running the model: \n{e}", fg='red'))
 
 @click.command()
 @click.option('--path', default='', help='Enter path to model file')
 def validate_command(path):
     try:
         _, m = load_metamodel_and_model_path(path)
-        # validate(m)   # useless for now
+        click.secho(click.style("Your model is correct!", fg='green'))
     except Exception as e:
-        print(f"An error occured while validating the model: \n{e}")
+        click.secho(click.style(f"An error occured while validating the model: \n{e}", fg='red'))
 
 
 cli.add_command(validate_command)
