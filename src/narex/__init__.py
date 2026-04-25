3094b93f86339e6bcf573eaf29082753537c8f65
Vasilije <vasilijezzzz@gmail.com>
2026-04-25 11:41:43 +0200

    feat: Use narex's `__init__.py` imports trick for cleaner import

---
+++ src/narex/__init__.py
@@ -0,0 +1,4 @@
+# Useful for cleaner importing
+from narex.utils.loader import load_metamodel_and_model_path
+from narex.utils.loader import load_metamodel_and_model_str
+from narex.generators.python import PythonEngine
