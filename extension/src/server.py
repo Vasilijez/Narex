090c1b778e9ca2b64b3e9aac0a5e72ab93497810
Vasilije <vasilijezzzz@gmail.com>
2026-04-28 19:22:55 +0200

    feat: Add initial workable version of autocompletion
    - Autocompletion for most rules works.
    - Suggestions are sorted based on the best match.
    - None invalid keyword is shown.

--- extension/src/server.py
+++ extension/src/server.py
@@ -1,3 +1,4 @@
+import re
 import logging
 from datetime import datetime
 from lsprotocol import types
@@ -52,6 +53,64 @@ def grammar_check(ls: NarexLanguageServer, params):
     )
     
 
+def print_msg(ls: NarexLanguageServer, message):
+    msg = ls.window_log_message(types.ShowMessageParams(
+                message=message,
+                type=types.MessageType.Info,
+            ))
+    return msg
+
+def catch_current_word(content: str) -> str:
+    match = re.search(r'(\w+)$', content)
+    word = match.group(1).lower() if match else ""
+    return word
+
+def slice_document_after_cursor(document, params) -> str:
+    offset = document.offset_at_position(params.position)
+    content = document.source[:offset]
+    return content
+
+def is_eligble_keyword(label, eligble_keywords):
+    if label in eligble_keywords:
+        return True
+    return False
+
+def get_label(ls, rule, eligble_keywords) -> str | None:
+
+    if hasattr(rule, 'to_match'):
+        if is_eligble_keyword(rule.to_match, eligble_keywords):
+            return rule.to_match
+    
+    elif hasattr(rule, 'rule_name'):
+        # TODO: Which case???
+        label = rule.rule_name.lower() if type(rule.rule_name) == "str" else rule.rule_name
+        if is_eligble_keyword(label, eligble_keywords):
+            return label
+    
+    elif hasattr(rule, 'name'):
+        label = rule.name
+        if is_eligble_keyword(label, eligble_keywords):
+            return label
+
+    return None
+
+def sort_by_starts_with(params, current_word, label) -> types.CompletionItem:
+    sort_priority = "0" if label.startswith(current_word) else "1"
+    current_pos = params.position
+    item = types.CompletionItem(
+        label=label,
+        kind=types.CompletionItemKind.Keyword,
+        filter_text=current_word,
+        sort_text=f"{sort_priority}"
+    )
+
+    return item 
+
+def preselect_first_item(items: list[types.CompletionItem]):
+    if len(items) >= 1:
+        first_item = items[0]
+        first_item.preselect = True
+
 @server.feature(types.TEXT_DOCUMENT_COMPLETION)
 def code_completion(ls: NarexLanguageServer, params):
 
@@ -59,22 +118,42 @@ def code_completion(ls: NarexLanguageServer, params):
  
     document_uri = params.text_document.uri
     document = ls.workspace.get_text_document(document_uri)
-    content = document.source
     items = []
 
+    # Cut off document content after cursor
+    document = slice_document_after_cursor(document, params)
+
+    # Catch current word
+    current_word = catch_current_word(document)
+
+    illegal_rules = {
+        'UnescapedString', 'ID', 'EdgeValue'
+    }
+
+    eligble_keywords = {k.lower() for k in ls.mm.namespaces['narex'].keys() if k.lower() not in illegal_rules}
+
     try:
-        ls.mm.model_from_str(content)
+        ls.mm.model_from_str(document)
+
+
     except TextXSyntaxError as e:
-        for r in e.expected_rules:
-            print(f"rule {r}")
-            if hasattr(r, 'rule_name') and r.rule_name:
-                label = r.rule_name
-            elif hasattr(r, 'to_match') and r.to_match:
-                label = r.to_match
-            items.append(types.CompletionItem(
-                label=label,
-                kind=types.CompletionItemKind.Keyword
-            ))
+
+        for rule in e.expected_rules:
+            label = get_label(ls, rule, eligble_keywords)
+
+            if label:
+                item = sort_by_starts_with(params, current_word, label)
+                
+                # Put one of the top priorty elements at the first position
+                # Do that as preparation for preselect
+                if item.sort_text.startswith("0"):
+                    items.insert(0, item)
+                
+                items.append(item)
+
+    except (TextXSemanticError, Exception) as e:
+        print_msg(ls, str(e))
+        pass
 
     return types.CompletionList(is_incomplete=False, items=items) # maybe True
 
