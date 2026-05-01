54a020220ed6658b315f8fdb13027d97276d64c3
Vasilije <vasilijezzzz@gmail.com>
2026-05-01 18:03:19 +0200

    feat: Add scaffolding for distributing tool - `flit`

--- src/narex/__init__.py
+++ src/narex/__init__.py
@@ -1,3 +1,6 @@
+
+__version__ = "0.1.0"
+
 # Useful for cleaner importing
 from narex.utils.loader import load_metamodel_and_model_path
 from narex.utils.loader import load_metamodel_and_model_str
@@ -7,3 +10,5 @@ from narex.grammar import GRAMMAR_PATH
 
 import os
 NAREX_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
